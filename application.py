6c4d1a68e1533c41f2c93a1a7150747c76575cd9
Thom Buyck <thom99.buyck@gmail.com>
2019-01-17 12:24:12 +0000

    register met database

--- application.py
+++ application.py
@@ -26,11 +26,40 @@ Session(app)
 
 # configure CS50 Library to use SQLite database
 db = SQL("sqlite:///sudokus.db")
-
 # def get_sudoku():
 random_sudoku = db.execute("SELECT sudoku FROM generated_sudokus ORDER BY random() LIMIT 1")
 # print(random_sudoku)
-for x in random_sudoku:
-    x = str(x).replace(".", " ")
-    print(x)
+for sudoku_cijfers in random_sudoku:
+    sudoku_cijfers = str(sudoku_cijfers).replace(".", " ")
+    print(sudoku_cijfers)
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user."""
+
+    # clear user_id
+    session.clear()
+    # wanneer gebruiker via Post
+    if request.method == "POST":
+        # check username
+        if not request.form.get("username"):
+            return apology("Vul een gebruikersnaam in")
+        # check wachtwoord
+        elif not request.form.get("password"):
+            return apology("Vul een wachtwoord in ")
+        # Check of 2x zelfde wachtwoord is ingevuld
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("wachtwoorden komen niet overeen!")
+        elif len(db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))) == 1:
+            return apology("confirmation password must match password")
+
+        db.execute("INSERT INTO users (username, hashed, email) VALUES (:username, :hashed, :email)",
+                username=request.form.get("username"), hashed =pwd_context.hash(request.form.get("password")), email=request.form.get("email"))
+
+        #veranderen
+        return redirect(url_for("register"))
+
+    else:
+        return render_template("register.html")
+
+    return apology("TODO")
