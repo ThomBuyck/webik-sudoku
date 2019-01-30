8244ddf988a0f39d37038e8157f80e24acf89d33
Stijn Hering <stijn@hering.nl>
2019-01-30 20:12:53 +0000

    update

--- static/checking.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-          $('a#test').bind('click', function() {
-            $.getJSON('/checking',
-                function(data) {
-              //do nothing
-            });
-            return false;
-          });
-        });
--- static/isnumber.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("explicit-block-txt").onkeypress = function(e) {
-    var chr = String.fromCharCode(e.which);
-    if (!("<>\"".indexOf(chr) >= 0))
-        return false;
-};
